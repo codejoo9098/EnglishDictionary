1e28671b6a67ba1f67e18521deca88472637212c
TaeWoonJeong <pig07911@naver.com>
2021-11-07 17:57:05 +0900

    단어 검색기능 완성

--- app/src/main/java/kr/co/project/zeroid/englishdictionary/MainActivity.java
+++ app/src/main/java/kr/co/project/zeroid/englishdictionary/MainActivity.java
@@ -2,19 +2,20 @@ package kr.co.project.zeroid.englishdictionary;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.provider.Settings;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import kr.co.project.zeroid.englishdictionary.addVoca.AddVocaActivity;
 import kr.co.project.zeroid.englishdictionary.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     MainViewModel viewModel;
+    String androidId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
                 navigateToSearchVocaPage();
             }
         });
+
+        // 안드로이드 아이디
+        androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        Toast.makeText(this,"androidId : "+androidId,Toast.LENGTH_SHORT).show();
     }
 
     void navigateToSearchVocaPage() {
--- app/src/main/java/kr/co/project/zeroid/englishdictionary/addVoca/AddVocaActivity.java
+++ app/src/main/java/kr/co/project/zeroid/englishdictionary/addVoca/AddVocaActivity.java
@@ -2,17 +2,22 @@ package kr.co.project.zeroid.englishdictionary.addVoca;
 
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,36 +25,47 @@ import kr.co.project.zeroid.englishdictionary.R;
 
 public class AddVocaActivity extends AppCompatActivity {
 
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference conditionRef = mRootRef.child("text");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_voca);
 
-        TextView addVocaEditText=findViewById(R.id.addVocaEditText);
-        Button addVocaButton=findViewById(R.id.searchButton);
-        TextView vocaResultTextView=findViewById(R.id.vocaResultTextView);
+        ListView listView=findViewById(R.id.addVocaListView);
+        TextView searchVocaEditText=findViewById(R.id.searchVocaEditText);
+        Button searchVocaButton=findViewById(R.id.searchVocaButton);
+        Button addVocaButton=findViewById(R.id.addVocaButton);
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        addVocaButton.setOnClickListener(new View.OnClickListener() {
+        searchVocaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String text= addVocaEditText.getText().toString();
-                Pattern onlyEnglish = Pattern.compile("^[a-zA-Z0-9]+$");
+                String text= searchVocaEditText.getText().toString();
+                Pattern onlyEnglish = Pattern.compile("^[a-zA-Z]+$");
                 Pattern onlyKorean=Pattern.compile("^[가-힣]+$");
                 if(onlyEnglish.matcher(text).matches()) {
-                    addVocaEditText.setText(null);
+                    searchVocaEditText.setText(null);
                     String result=searchEnglish(text);
-                    vocaResultTextView.setText(result);
+                    showResult(listView, result);
                 } else if(onlyKorean.matcher(text).matches()) {
-                    addVocaEditText.setText(null);
+                    searchVocaEditText.setText(null);
                     String result=searchKorean(text);
-                    vocaResultTextView.setText(result);
+                    showResult(listView, result);
                 } else {
-                    addVocaEditText.setText(null);
+                    searchVocaEditText.setText(null);
                     Toast.makeText(getApplicationContext(),"영어만 입력하거나 한글만 입력하세요.",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        addVocaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO 실시간 데이터베이스에 선택된 단어 넣기
             }
         });
     }
@@ -73,7 +89,34 @@ public class AddVocaActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         Element elem=doc.getElementsByAttributeValue("data-tiara-layer","word eng").select("ul[class=list_search]").first();
-        String result=elem.text();
-        return result;
+        try {
+            return elem.text();
+        } catch(NullPointerException e) {
+            return "";
+        }
+    }
+
+    private String[] trimResult(String result) {
+        return result.replaceFirst("[0-9].","").replaceAll("[0-9].","\n").split("\n");
+    }
+
+    private void showResult(ListView listView, String result) {
+        String[] resultArray=trimResult(result);
+        if(resultArray[0]!="") {
+            ArrayList<AddVocaListData> listViewData = new ArrayList<>();
+            for (int i=0; i<resultArray.length; i++)
+            {
+                AddVocaListData listData = new AddVocaListData();
+                listData.voca = resultArray[i];
+                listViewData.add(listData);
+            }
+            ListAdapter oAdapter = new AddVocaCustomView(listViewData);
+            listView.setAdapter(oAdapter);
+        } else {
+            ArrayList<AddVocaListData> listViewData = new ArrayList<>();
+            ListAdapter oAdapter = new AddVocaCustomView(listViewData);
+            listView.setAdapter(oAdapter);
+            Toast.makeText(getApplicationContext(),"검색 결과가 없습니다.",Toast.LENGTH_SHORT).show();
+        }
     }
 }
