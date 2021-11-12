45c6a2a71b1402e7b808d9b6387a3a532c217a16
주재완 <yjjw2000@naver.com>
2021-11-12 21:22:23 +0900

    AndroidId를 MainActivity에 추가

--- app/src/main/java/kr/co/project/zeroid/englishdictionary/MainActivity.java
+++ app/src/main/java/kr/co/project/zeroid/englishdictionary/MainActivity.java
@@ -9,9 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import dagger.hilt.android.AndroidEntryPoint;
 import kr.co.project.zeroid.englishdictionary.addVoca.AddVocaActivity;
 import kr.co.project.zeroid.englishdictionary.databinding.ActivityMainBinding;
+import kr.co.project.zeroid.englishdictionary.vocatest.settingvoca.SettingVocaTestActivity;
 
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     MainViewModel viewModel;
@@ -31,13 +35,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // 안드로이드 아이디
+        viewModel.navigateToVocaTest.observe(this, new Observer<Void>() {
+            @Override
+            public void onChanged(Void unused) {
+                navigateToVocaTestPage();
+            }
+        });
+
         androidId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-        Toast.makeText(this,"androidId : "+androidId,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"androidId : "+androidId, Toast.LENGTH_SHORT).show();
     }
 
     void navigateToSearchVocaPage() {
         Intent intent = new Intent(this, AddVocaActivity.class);
+        startActivity(intent);
+    }
+
+    void navigateToVocaTestPage() {
+        Intent intent = new Intent(this, SettingVocaTestActivity.class);
         startActivity(intent);
     }
 }
