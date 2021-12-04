42913d30f6ba0225b56dd1b12476e13a8561f6d2
주재완 <yjjw2000@naver.com>
2021-12-04 22:08:42 +0900

    나만의 단어장 디자인 수정, build.gradle 수정

--- app/src/main/java/kr/co/project/zeroid/englishdictionary/application/DictionaryApplication.java
+++ app/src/main/java/kr/co/project/zeroid/englishdictionary/application/DictionaryApplication.java
@@ -2,9 +2,6 @@ package kr.co.project.zeroid.englishdictionary.application;
 
 import android.app.Application;
 
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
 public class DictionaryApplication extends Application {
 
 }
