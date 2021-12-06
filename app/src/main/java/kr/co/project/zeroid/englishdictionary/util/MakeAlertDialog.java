91bdd338087b0013243b571b298c6c9b79095cb3
TaeWoonJeong <pig07911@naver.com>
2021-12-07 02:08:51 +0900

    단어검색시 버그 수정

--- app/src/main/java/kr/co/project/zeroid/englishdictionary/util/MakeAlertDialog.java
+++ app/src/main/java/kr/co/project/zeroid/englishdictionary/util/MakeAlertDialog.java
@@ -11,7 +11,7 @@ import kr.co.project.zeroid.englishdictionary.singleton.SingletonVocaMap;
 public class MakeAlertDialog {
     public static AlertDialog makeAlertDialog(Context context) {
         AlertDialog.Builder builder=new AlertDialog.Builder(context);
-        builder.setTitle("접속실패").setMessage("통신상태가 원활하지 않습니다.\n이동통신사 혹은 와이아피의 상태를 확인하시기 바랍니다.");
+        builder.setTitle("접속실패").setMessage("통신상태가 원활하지 않습니다.\n이동통신사 혹은 와이파이의 상태를 확인하시기 바랍니다.");
         builder.setPositiveButton("재연결", new DialogInterface.OnClickListener(){
             @Override
             public void onClick(DialogInterface dialog, int id) {
